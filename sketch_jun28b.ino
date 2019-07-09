a2cc2775dff924816d472c80b21b24236512566c
avo2003 <50109997+avo2003@users.noreply.github.com>
2019-07-10 01:45:54 +0400

    Add files via upload

---
+++ sketch_jun28b.ino
@@ -0,0 +1,19 @@
+int randNumber;
+int led =6;
+void setup() {
+pinMode(led, OUTPUT);
+ Serial.begin(9600);
+
+
+}
+
+void loop() {
+
+  randNumber = random(0,250);
+
+ analogWrite(led, randNumber); 
+ Serial.println(randNumber);
+ delay(1000);
+
+
+}
